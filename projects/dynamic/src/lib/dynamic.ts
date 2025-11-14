74aad16ed19efca2e935880baab593f543505411
pepesoriagarcia99 <pepesoriagarcia99@gmail.com>
2025-11-14 09:32:44 +0100

    feat: inicio proyecto wc

---
+++ projects/dynamic/src/lib/dynamic.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dyn-dynamic',
+  imports: [],
+  template: `
+    <p>
+      dynamic works!
+    </p>
+  `,
+  styles: ``,
+})
+export class Dynamic {
+
+}
---
+++ projects/dynamic/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of dynamic
+ */
+
+export * from './lib/dynamic';
