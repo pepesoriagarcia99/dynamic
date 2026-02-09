44a9340a6bfd825e307e7bdfeeb6b6a2a2ccbd3e
pepesoriagarcia99 <pepesoriagarcia99@gmail.com>
2026-02-09 17:42:54 +0100

    feat: simplificacion

--- FRONTEND/src/modules/table/models/RowData.ts
+++ FRONTEND/src/modules/table/models/RowData.ts
@@ -1,6 +1,6 @@
 export type RowData = {
   [key: string]: any; // resto de datos de la fila
-  __ctx: {
-    isSelected?: boolean; // Indica si la fila está seleccionada
-  };
+  // __ctx: {
+  //   isSelected?: boolean; // Indica si la fila está seleccionada
+  // };
 };
